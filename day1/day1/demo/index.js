4c3721731bbdb36aed902317380a689bbca926b3
sheetus <47025393+sheetus@users.noreply.github.com>
2022-12-03 20:58:26 +0200

    second lab

---
+++ day1/day1/demo/index.js
@@ -0,0 +1,145 @@
+//let vs var
+//1-block scope
+// let age=10;
+// for(var i=0;i<5;i++)
+// {
+//     console.log('aswan')
+// }
+
+// console.log(i);
+//2-hositing
+// dispay()
+// console.log(user); //error 
+// var user="ahmed"
+// console.log(x);
+// var x=dispay();
+// console.log(x)
+
+// function dispay() //hosting 
+// {
+//     alert("")
+// }
+// console.log(show);
+// show();
+
+// function showX()
+// {
+//     alert('');
+// }
+// var x=showX;
+// console.log(x);
+// showX()
+// var show=function()
+// {
+//     console.log('show');
+// }
+// show();
+// console.log(user)
+// let user="sayed";
+
+//3-redcaler
+// var user="ahmed";
+// var user="sayed";
+// console.log(user)
+// let age=10;
+// let age=20;
+// age=30;
+// const Pi=3.14;
+// Pi=233;
+// console.log(Math.floor())
+/////////////////////////dataTypes////////////////////////////////////////
+// var x=Symbol("ddd");
+// var z=Symbol('ddd')
+// console.log(typeof x)
+// var age=20;
+// var age2=age;
+
+// age=10;
+// console.log(age);//10
+// console.log(age2)//20
+
+// var obj={
+//     name:"ahmed",
+//     age:30,
+//     male:true
+// }
+// var o2=obj;
+
+// obj.name="ali";
+
+// console.log(typeof obj);//ali
+// console.log(o2);//ali
+
+
+// console.log(10+"iti");// implicit
+// console.log(10>5>2); //
+// var num="10";
+// var x=parseInt(true);
+// console.log(typeof num);
+// console.log(typeof x);
+// var num= prompt("enter your year");
+// var num1=parseInt(num);
+// var num2=Number(num);
+// // num=num-0 // => num*1
+// console.log(num1)
+// console.log(num2)
+// // console.log(num+20);
+
+var gender=prompt("enter your gender");
+// if(gender== "male")
+// {
+//     console.log("mr")
+// }
+// else if(gender=="female")
+// {
+//     console.log("Ms");
+// }
+// else
+// {
+//     console.log("error");
+// }
+// if(gender=="male")
+// {
+//     console.log("sss");
+
+// }
+
+// if(gender=="female"){
+
+// }
+
+// switch(gender)
+// {
+//     case "male":// gender =="male"
+//         console.log("MR")
+//         break;
+//     case "female":
+//         console.log("MS")
+//         break;
+//     default:
+//         console.log("error")
+// }
+
+//  start   endcond  steps 
+// for(var i=0;i<10;i++)
+// {
+// console.log(i);
+// }
+// var i=0;
+// while(i<10)
+// {
+//     console.log(i);
+//     i++;
+// }
+// var num=parseInt("10");
+
+// console.log('iti'+5);
+// console.log(num)
+// var i=6;
+// do{
+//     console.log(i)
+//     i++;
+// }while(i<5)
+
+
+
---
+++ day1/day1/old/index.js
@@ -0,0 +1,100 @@
+
+// var age=20;
+
+// console.log(age);
+
+
+// function solve()
+// {
+//     var user="iti";
+
+//     console.log(user);
+// }
+
+
+// solve()
+// console.log(user);
+
+// age="iti aswan";
+
+// console.log(age);
+// sdwdjhwdhwdhuh
+///////////////////////////////////////////////////////////////////////////////////////
+
+
+// var num1=10;
+// var num2=20;
+
+// console.log(num1+num2);
+// console.log(num1-num2);
+
+// var num3=num1++; // => num3=10; then incr num1 =>11
+//num1=11;
+// var num4=num1;
+// console.log("num3:",num3);
+// console.log("num1:",num1);
+// console.log("num4:",num4);
+// console.log(num1++); // num1=num1+1 // post 
+// console.log(++num2); // pre
+
+// var rem=25%5; //   27-25=2 
+// console.log(rem);
+///////////////////////////////////////////////////////////////////
+// var flag=20<30;
+// var flag2=20>=25;
+// var flag3=20==30;  //  20===30
+// var flag4=20=="20"; //  
+// console.log(flag4);
+// var flag5=20==="20";
+// console.log(flag5);
+// var num5=20;
+// console.log(num5+"iti");
+
+// var flag6=20!=="20"; 
+// console.log(flag6);
+//////////////////////////////////////////////////
+
+// var num1=20;
+// var num2=10;
+// var num3=7;
+// var flag1=num1>num2&&num1>num3; /// true && true => true
+// var flag2=num2>num1&&num2>num3; //  false && true => flase
+// var flag3=num3>num1&&num3>num2; // false && false => false 
+
+// var flag4=true || false;
+// var flag5=false|| false;
+// var flag6=true || true;
+// console.log(flag4);
+// console.log(flag5);
+// console.log(flag6);
+// var flag7=!false; // =>  -
+// console.log(flag7)
+/////////////////////////////////////
+// console.log(flag);
+// console.log(flag2);
+
+/////////////////////////////////////////////////////////
+
+// alert("hi in iti aswan");
+
+// confirm('are you male?');
+
+var username= prompt('enter your name');
+var year=parseInt( prompt("enter your brith year")); // NaN not a number
+
+
+console.log(year);
+console.log(username)
+
+console.log(year+10);
+
+
+
+
+
+
+
+
+
+
+
